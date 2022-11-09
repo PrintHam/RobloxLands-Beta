8e5b68dbad58c981b002ad0c3aa0e9a15166fa74
Wilfred Chow <wilfredc1223@gmail.com>
2022-11-08 19:48:04 -0800

    Beta Version of the Lander

---
+++ static/js/Redirect.js
@@ -0,0 +1,15 @@
+function mobileRedirect(){
+    // const af_dp = "roblox%3A%2F%2FplaceId%3D7682549746%26launchData%3D1";
+    // const af_web_dp = "https%3A%2F%2Fwww.roblox.com%2Fgames%2Fstart%3FlaunchData%3D1%26placeId%3D7682549746";
+    // const domain = "https://ro.blox.com/Ebh5?";
+
+    window.open("https://ro.blox.com/Ebh5?af_dp=roblox%3A%2F%2FplaceId%3D7682549746%26launchData%3D%257B%2522roomId%2522%253A%25202%257D&af_web_dp=https%3A%2F%2Fwww.roblox.com%2Fgames%2Fstart%3FplaceId%3D7682549746%26launchData%3D%257B%2522roomId%2522%253A%25202%257D ");
+}
+function mobile2Redirect(){
+    window.location.replace("https://ro.blox.com/Ebh5?af_dp=roblox%3A%2F%2FplaceId%3D7682549746%26launchData%3D1%&af_web_dp=https%3A%2F%2Fwww.roblox.com%2Fgames%2Fstart%3FlaunchData%3D1%26placeId%3D7682549746");
+}
+function desktopRedirect(){
+    const desktopLink = "https://www.roblox.com/games/start?launchData=1&placeId=7682549746";
+
+    window.location.replace(desktopLink);
+}
